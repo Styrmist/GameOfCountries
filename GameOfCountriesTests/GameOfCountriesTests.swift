bd2900e2679cea3de8b15b94b6e60641e0af390a
Laziest <_limitless@tuta.io>
2021-01-24 03:22:56 +0200

    add tests target

---
+++ GameOfCountriesTests/GameOfCountriesTests.swift
@@ -0,0 +1,33 @@
+//
+//  GameOfCountriesTests.swift
+//  GameOfCountriesTests
+//
+//  Created by Kirill Danilov on 24/01/2021.
+//
+
+import XCTest
+
+class GameOfCountriesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
